8a5fa695f0055b997d96cc568580b73e6b41beb4
Jae-Yong Choi <jayong93@gmail.com>
2018-05-13 01:30:38 +0900

    스크립트를 추가한 nmt 모듈 추가

---
+++ nmt/train.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SOURCE_DIR=$(cd $(dirname "$0") && pwd)
+VOLUME_DIR=/nmt
+
+docker run --rm -v ${SOURCE_DIR}:${VOLUME_DIR} tensorflow/tensorflow:nightly-devel-py3 bash -c "export PYTHONIOENCODING=UTF-8 && cd /nmt && python3 -m nmt.nmt \
+    --src=title --tgt=comment \
+    --vocab_prefix=/nmt/train/vocab \
+    --train_prefix=/nmt/train/train \
+    --dev_prefix=/nmt/train/dev  \
+    --test_prefix=/nmt/train/test \
+    --out_dir=/nmt/save/model \
+    --num_train_steps=12000 \
+    --steps_per_stats=100 \
+    --num_layers=2 \
+    --num_units=128 \
+    --dropout=0.5 \
+    --share_vocab \
+    --metrics=bleu | tee /nmt/logs/train.log"
