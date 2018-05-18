e441e52655625f08a296aa7d065ca8879e4002a8
Jae-Yong Choi <jayong93@gmail.com>
2018-05-18 15:09:31 +0900

    nmt 학습할 때 시간별로 저장하도록 변경

--- nmt/infer.sh
+++ nmt/infer.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-INPUT_FILE="${1:?give input text file to decode}"
-OUTPUT_FILE="${2:?give output file name to store decoded result}"
+MODEL_DIR=${1:?give a directory in which saved model is}
+INPUT_FILE="${2:?give an input text file to decode}"
+OUTPUT_FILE="${3:?give an output file name to store decoded result}"
 
 # get full path
-INPUT_FILE=$(cd $(dirname "${INPUT_FILE}") && pwd)/${INPUT_FILE}
-OUTPUT_FILE=$(cd $(dirname "${OUTPUT_FILE}") && pwd)/${OUTPUT_FILE}
+INPUT_FILE=$(readlink -f "${INPUT_FILE}")
+OUTPUT_FILE=$(readlink -f "${OUTPUT_FILE}")
+MODEL_DIR=$(readlink -f ${MODEL_DIR})
+CKPT_FILE=$(ls ${MODEL_DIR} | grep "ckpt.\+\.index" | sort -V -r | head -1)
 
 SOURCE_DIR=$(cd $(dirname "$0") && pwd)
 VOLUME_DIR=/nmt
 
 INPUT_FILE=${INPUT_FILE//${SOURCE_DIR}/${VOLUME_DIR}}
 OUTPUT_FILE=${OUTPUT_FILE//${SOURCE_DIR}/${VOLUME_DIR}}
+MODEL_DIR=${MODEL_DIR//${SOURCE_DIR}/${VOLUME_DIR}}
+
 
 docker run --rm -v ${SOURCE_DIR}:${VOLUME_DIR} tensorflow/tensorflow:nightly-devel-py3 bash -c "cd /nmt && python3 -m nmt.nmt \
     --src=title --tgt=comment \
-    --ckpt=${VOLUME_DIR}/save/model/translate.ckpt-11000 \
-    --out_dir=${VOLUME_DIR}/save/model \
+    --ckpt=${MODEL_DIR}/${CKPT_FILE} \
+    --out_dir=${MODEL_DIR} \
     --vocab_prefix=${VOLUME_DIR}/train \
+    --share_vocab \
     --inference_input_file="${INPUT_FILE}" \
     --inference_output_file="${OUTPUT_FILE}""
