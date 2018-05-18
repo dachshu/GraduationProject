48798e8c27de26c45900914fd55ac68800e152d7
Jae-Yong Choi <jayong93@gmail.com>
2018-05-18 15:09:31 +0900

    nmt 학습할 때 시간별로 저장하도록 변경

--- nmt/train.sh
+++ nmt/train.sh
@@ -2,6 +2,7 @@
 
 SOURCE_DIR=$(cd $(dirname "$0") && pwd)
 VOLUME_DIR=/nmt
+CURR_TIME=$(date "+%F-%H-%M-%S")
 
 docker run --rm -v ${SOURCE_DIR}:${VOLUME_DIR} tensorflow/tensorflow:nightly-devel-py3 bash -c "export PYTHONIOENCODING=UTF-8 && cd /nmt && python3 -m nmt.nmt \
     --src=title --tgt=comment \
@@ -9,11 +10,11 @@ docker run --rm -v ${SOURCE_DIR}:${VOLUME_DIR} tensorflow/tensorflow:nightly-dev
     --train_prefix=/nmt/train/train \
     --dev_prefix=/nmt/train/dev  \
     --test_prefix=/nmt/train/test \
-    --out_dir=/nmt/save/model \
+    --out_dir=/nmt/save/model-${CURR_TIME} \
     --num_train_steps=12000 \
     --steps_per_stats=100 \
     --num_layers=2 \
     --num_units=128 \
     --dropout=0.5 \
     --share_vocab \
-    --metrics=bleu | tee /nmt/logs/train.log"
+    --metrics=bleu | tee /nmt/logs/train-${CURR_TIME}.log"
