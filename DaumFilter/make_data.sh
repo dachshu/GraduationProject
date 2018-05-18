d5f6de252ceff9c741c6ca25bc460f51e4cb9802
Jae-Yong Choi <jayong93@gmail.com>
2018-05-18 15:11:22 +0900

    DaumFilter 데이터 만드는 스크립트를 좀 더 유연하게 변경

--- DaumFilter/make_data.sh
+++ DaumFilter/make_data.sh
@@ -5,7 +5,8 @@ OUT_DIR=${SCRIPT_DIR}/output
 OUT_JSON_NAME=${OUT_DIR}/output.json
 
 mkdir -p ${OUT_DIR}
-./filter.py ${ARCHIVES_DIR} -o ${OUT_JSON_NAME} 0<&0
-./make_training_input.py space ${OUT_JSON_NAME} --out_dir ${OUT_DIR} --separate_title
-./separate_dataset.py ${OUT_DIR}/output.title ${OUT_DIR}/output.comment ${OUT_DIR}/output.vocab --out_dir ${OUT_DIR}
+${SCRIPT_DIR}/filter.py ${ARCHIVES_DIR} -o ${OUT_JSON_NAME} 0<&0
+$(cd ${SCRIPT_DIR} && python3 process_data.py && mv *.txt -t ${OUT_DIR})
+sed -i "/^$/d" ${OUT_DIR}/*.txt
+${SCRIPT_DIR}/separate_dataset.py ${OUT_DIR}/title.txt ${OUT_DIR}/comment.txt ${OUT_DIR}/vocab.txt --out_dir ${OUT_DIR}
 cp -f ${OUT_DIR}/*.title ${OUT_DIR}/*.comment -t ${SCRIPT_DIR}/../nmt/train
