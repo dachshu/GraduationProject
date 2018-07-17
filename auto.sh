6150600c7f651d12ca6918f140faf396ff212e2b
Jae-Yong Choi <jayong93@gmail.com>
2018-07-17 18:26:28 +0900

    댓글 크롤링&생성 자동화 스크립트 구현

--- auto.sh
+++ auto.sh
@@ -1,36 +1,6 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
-CRAWLER_DIR=${SCRIPT_DIR}/crawler
-CRAWLED_DATA_DIR=${CRAWLER_DIR}/crawled_data/daum_news
-FILTER_DIR=${SCRIPT_DIR}/DaumFilter
-CHAR_RNN_DIR=${SCRIPT_DIR}/char-rnn-tensorflow
-NMT_DIR=${SCRIPT_DIR}/nmt
-CRAWL_DATE=$(date '+%Y%m%d' -d "yesterday")
 
-# CharRNN 학습 데이터 준비
-echo "=== Start Crawling ${CRAWL_DATE} ==="
-${CRAWLER_DIR}/DaumCrawler.py --date ${CRAWL_DATE}
-
-echo "=== Filtering Crawled Data ==="
-echo -e "${CRAWLED_DATA_DIR}/${CRAWL_DATE}" | ${FILTER_DIR}/make_data.sh
-
-echo "=== Copying Filtered Data ==="
-mkdir -p ${CHAR_RNN_DIR}/data/news
-cp ${FILTER_DIR}/output/char_rnn_data.txt ${CHAR_RNN_DIR}/data/news/input.txt
-
-# NMT 학습 데이터 준비
-NMT_ADDTIONAL_DATE=$(date '+%Y%m%d' -d "2 day ago")
-echo -e "${CRAWLED_DATA_DIR}/${CRAWL_DATE}\n${CRAWLED_DATA_DIR}/${NMT_ADDTIONAL_DATE}" | ${FILTER_DIR}/make_data.sh
-cp ${FILTER_DIR}/output/*.comment ${NMT_DIR}/train/
-cp ${FILTER_DIR}/output/*.title ${NMT_DIR}/train/
-
-echo "=== Start Learning ==="
-${CHAR_RNN_DIR}/run.py train &
-PID1=$!
-${NMT_DIR}/train.sh &
-PID2=$!
-
-wait ${PID1} ${PID2}
-
-echo "=== Learning Complete ==="
+${SCRIPT_DIR}/train.sh
+${SCRIPT_DIR}/generate.sh
---
+++ generate.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+CHAR_RNN_DIR=${SCRIPT_DIR}/char-rnn-tensorflow
+NMT_DIR=${SCRIPT_DIR}/nmt
+
+echo "=== Generating Times ==="
+TIME_GENERATOR_DIR=${SCRIPT_DIR}/CommentTimeGenerator
+cd ${TIME_GENERATOR_DIR}
+LATEST_TIME=$(cat latest_generated_time)
+GENERATED_TIMES=$(./TimeModel.py sample ${LATEST_TIME})
+
+echo "=== Generating Comments ==="
+TODAY=$(date '+%Y-%m-%d')
+for t in ${GENERATED_TIMES}; do
+    HOUR=$(echo "${t}/3600" | bc)
+    MINUTE=$(echo "${t}%3600/60" | bc)
+    SECOND=$(echo "${t}%3600%60" | bc)
+
+    TARGET_TIME=$(date --date="${TODAY} ${HOUR}:${MINUTE}:${SECOND}" '+%s')
+    CURRENT_TIME=$(date '+%s')
+
+    if [ $(echo "${TARGET_TIME}<${CURRENT_TIME}" | bc) -eq 1 ]; then
+        continue
+    fi
+
+    PERIOD_TO_SLEEP=$((TARGET_TIME-CURRENT_TIME))
+    echo "Sleep for ${PERIOD_TO_SLEEP}s"
+    sleep ${PERIOD_TO_SLEEP}
+
+    # get a news article
+    ARTICLE_TITLES=$(${SCRIPT_DIR}/GetDaumMainNews.py)
+    SELECTED_TITLE=$(echo "${ARTICLE_TITLES}" | shuf -n 1)
+    # generate comments of the article via char-rnn and seq2seq model
+    echo "Generate a comment from the title: ${SELECTED_TITLE}"
+    echo "${SELECTED_TITLE}" > ${CHAR_RNN_DIR}/input_for_generation.txt
+    ${CHAR_RNN_DIR}/run.py infer --input_file ${CHAR_RNN_DIR}/input_for_generation.txt
+    CRNN_RESULT="$(cat ${CHAR_RNN_DIR}/infer_output.txt)"
+
+    echo "${SELECTED_TITLE}" > ${NMT_DIR}/input_for_generation.txt
+    ${NMT_DIR}/infer.sh ${NMT_DIR}/save/model ${NMT_DIR}/input_for_generation.txt ${NMT_DIR}/infer_output.txt
+    NMT_RESULT="$(cat ${NMT_DIR}/infer_output.txt)"
+    # TODO:
+    # write the result to a file
+
+done
