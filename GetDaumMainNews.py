03697a6fa187d69ab126ec4658e8797bce352002
Jae-Yong Choi <jayong93@gmail.com>
2018-07-17 18:35:56 +0900

    다음 뉴스 제목을 가져올 때 모바일 페이지에서 가져오도록 변경

--- GetDaumMainNews.py
+++ GetDaumMainNews.py
@@ -5,20 +5,27 @@ from bs4 import BeautifulSoup
 
 
 def get_article_title(url):
+    ret_url = url
     rq = requests.get(url)
     soup = BeautifulSoup(rq.text, 'html.parser')
-    return soup.find('h3', class_='tit_view').text
+    if 'm.media' in url:
+        article = soup.select('div.item_mainnews div.cont_thumb a')
+        ret_url = article[0]['href']
+        rq = requests.get(ret_url)
+        soup = BeautifulSoup(rq.text, 'html.parser')
+
+    return (soup.find('h3', class_='tit_view').text, ret_url)
 
 
 if __name__ == '__main__':
-    rq = requests.get('http://media.daum.net')
+    rq = requests.get('https://m.daum.net')
     soup = BeautifulSoup(rq.text, 'html.parser')
   
     articles = []
-    articles += soup.select('ul.list_view strong.tit_g a.link_txt')
-    articles += soup.select('ul.list_headline strong.tit_g a.link_txt')
-   
-    titles = [get_article_title(article['href']) for article in articles]
+    articles += soup.select('div.out_ibox div:nth-of-type(1) ul.list_txt a')
+
+    titles = [get_article_title(article['href']) for article in articles[:5]]
     
     for title in titles:
-        print(title)
+        print(title[0])
+        print(title[1])
