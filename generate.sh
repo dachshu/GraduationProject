46dcd44bb10d0b8741aa04ab86470d53746b2b87
Jae-Yong Choi <jayong93@gmail.com>
2018-07-18 20:01:08 +0900

    comment 생성 후 twitter에 업로드 하도록 수정

--- generate.sh
+++ generate.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 CHAR_RNN_DIR=${SCRIPT_DIR}/char-rnn-tensorflow
 NMT_DIR=${SCRIPT_DIR}/nmt
+TWEET_UPLOADER_DIR=${SCRIPT_DIR}/TweetUploader
 
 echo "=== Generating Times ==="
 TIME_GENERATOR_DIR=${SCRIPT_DIR}/CommentTimeGenerator
@@ -30,19 +31,23 @@ for t in ${GENERATED_TIMES}; do
 
     # get a news article
     ARTICLE_TITLES_LINKS=$(${SCRIPT_DIR}/GetDaumMainNews.py)
-    SELECTED_TITLE=$(echo "${ARTICLE_TITLES_LINKS}" | awk 'NR % 2 == 1' | shuf -n 1)
+    INDEX=$(shuf -i 1-5 -n 1)
+    SELECTED_TITLE=$(echo "${ARTICLE_TITLES_LINKS}" | awk "NR == (${INDEX}*2-1)")
+    SELECTED_LINK=$(echo "${ARTICLE_TITLES_LINKS}" | awk "NR == ${INDEX}*2")
+    echo -e "${SELECTED_TITLE}\n${SELECTED_LINK}\n" > /tmp/generated_comments.txt
+
     # generate comments of the article via char-rnn and seq2seq model
     echo "Generate a comment from the title: ${SELECTED_TITLE}"
     echo "${SELECTED_TITLE}" > ${CHAR_RNN_DIR}/input_for_generation.txt
-    ${CHAR_RNN_DIR}/run.py infer --input_file ${CHAR_RNN_DIR}/input_for_generation.txt
-    CRNN_RESULT="$(cat ${CHAR_RNN_DIR}/infer_output.txt)"
+    python3 ${CHAR_RNN_DIR}/sample.py --save_dir ${CHAR_RNN_DIR}/save/news --prime "$(head -1 ${CHAR_RNN_DIR}/input_for_generation.txt)" \
+        --output_file ${CHAR_RNN_DIR}/infer_output.txt
+    cat ${CHAR_RNN_DIR}/infer_output.txt >> /tmp/generated_comments.txt
 
     echo "${SELECTED_TITLE}" > ${NMT_DIR}/input_for_generation.txt
     ${NMT_DIR}/infer.sh ${NMT_DIR}/save/model ${NMT_DIR}/input_for_generation.txt ${NMT_DIR}/infer_output.txt
-    NMT_RESULT="$(cat ${NMT_DIR}/infer_output.txt)"
-    # TODO:
-    # upload comment and original news web link to twitter
+    cat ${NMT_DIR}/infer_output.txt >> /tmp/generated_comments.txt
 
+    cat /tmp/generated_comments.txt | ${TWEET_UPLOADER_DIR}/TweetUploader.py -k ${TWEET_UPLOADER_DIR}/twitter_key
 done
 
 cd ${TIME_GENERATOR_DIR} && echo "${GENERATED_TIMES}" | tail -1 > latest_generated_time
