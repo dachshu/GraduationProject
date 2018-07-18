d33432ce09e8d249c9a6bd81bc459f401c90b7a9
Jae-Yong Choi <jayong93@gmail.com>
2018-07-18 18:44:58 +0900

    kor-char-rnn-tensorflow를 사용하도록 변경

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 CRAWLER_DIR=${SCRIPT_DIR}/crawler
 CRAWLED_DATA_DIR=${CRAWLER_DIR}/crawled_data/daum_news
 FILTER_DIR=${SCRIPT_DIR}/DaumFilter
-CHAR_RNN_DIR=${SCRIPT_DIR}/char-rnn-tensorflow
+CHAR_RNN_DIR=${SCRIPT_DIR}/kor-char-rnn-tensorflow
 NMT_DIR=${SCRIPT_DIR}/nmt
 CRAWL_DATE=$(date '+%Y%m%d' -d "yesterday")
 
@@ -19,6 +19,9 @@ echo "=== Copying Filtered Data ==="
 mkdir -p ${CHAR_RNN_DIR}/data/news
 cp ${FILTER_DIR}/output/char_rnn_data.txt ${CHAR_RNN_DIR}/data/news/input.txt
 
+# CharRNN 기존 모델 백업
+tar -czf ${CHAR_RNN_DIR}/save/news.bak.tgz ${CHAR_RNN_DIR}/save/news/
+
 # NMT 학습 데이터 준비
 NMT_ADDTIONAL_DATE=$(date '+%Y%m%d' -d "2 day ago")
 echo -e "${CRAWLED_DATA_DIR}/${CRAWL_DATE}\n${CRAWLED_DATA_DIR}/${NMT_ADDTIONAL_DATE}" | ${FILTER_DIR}/make_data.sh
@@ -26,7 +29,7 @@ cp ${FILTER_DIR}/output/*.comment ${NMT_DIR}/train/
 cp ${FILTER_DIR}/output/*.title ${NMT_DIR}/train/
 
 echo "=== Start Learning ==="
-${CHAR_RNN_DIR}/run.py train &
+${CHAR_RNN_DIR}/train.sh --init_from ${CHAR_RNN_DIR}/save/news &
 PID1=$!
 ${NMT_DIR}/train.sh &
 PID2=$!
