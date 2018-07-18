59f1abb8050671212c6751de66cd9310bd62d36b
Jae-Yong Choi <jayong93@gmail.com>
2018-07-18 20:00:37 +0900

    kor-char-rnn 문제에 맞춰 수정

--- kor-char-rnn-tensorflow/sample.py
+++ kor-char-rnn-tensorflow/sample.py
@@ -22,6 +22,8 @@ def main():
     parser.add_argument('--sample', type=int, default=1,
                         help='0 to use max at each timestep, 1 to sample at '
                              'each timestep, 2 to sample on spaces')
+    parser.add_argument('--output_file', type=argparse.FileType('w'),
+                        help="file to save sampling output")
 
     args = parser.parse_args()
     sample(args)
@@ -39,8 +41,11 @@ def sample(args):
         ckpt = tf.train.get_checkpoint_state(args.save_dir)
         if ckpt and ckpt.model_checkpoint_path:
             saver.restore(sess, ckpt.model_checkpoint_path)
-            print(model.sample(sess, chars, vocab, args.n, args.prime,
-                               args.sample))
+            result = model.sample(sess, chars, vocab, args.n, args.prime, args.sample)
+            if args.output_file is None:
+                print(result)
+            else:
+                args.output_file.write(result)
 
 if __name__ == '__main__':
     main()
