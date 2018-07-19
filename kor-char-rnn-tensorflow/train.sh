63987d6a5cc3972fb281636db3366ab8ee382171
Jae-Yong Choi <jayong93@gmail.com>
2018-07-19 17:32:17 +0900

    CharRNN 학습 epoch 수 감소

--- kor-char-rnn-tensorflow/train.sh
+++ kor-char-rnn-tensorflow/train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#python3 train.py --data_dir data/news/ --save_dir save/news/ --rnn_size 1024 --num_layers 3 --batch_size 120 "$@"
-python3 train.py --data_dir data/news/ --save_dir save/news/ --rnn_size 1024 --num_layers 3 --batch_size 60 "$@"
+#python3 train.py --data_dir data/news/ --save_dir save/news/ --rnn_size 1024 --num_layers 3 --num_epochs 100 --batch_size 120 "$@"
+python3 train.py --data_dir data/news/ --save_dir save/news/ --rnn_size 1024 --num_layers 3 --num_epochs 100 --batch_size 60 "$@"
