f798ef7d8f60f3a612c0af8c251924dfee98fb96
Jae-Yong Choi <jayong93@gmail.com>
2018-07-19 23:00:39 +0900

    다음 crawler 옵션을 합쳐서 적용

--- crawler/DaumCrawler.py
+++ crawler/DaumCrawler.py
@@ -5,116 +5,103 @@ import datetime
 import json
 import argparse
 import sys
+from multiprocessing import Pool, Queue
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException, NoSuchElementException, StaleElementReferenceException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-script_path = os.path.dirname(os.path.realpath(sys.argv[0]))
+SCRIPT_PATH = os.path.dirname(os.path.realpath(sys.argv[0]))
 
 class DaumCrawler:
     def __init__(self):
-        os.environ['MOZ_HEADLESS'] = '1'
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(0)
         self.base_url = u'http://media.daum.net/ranking/bestreply/?regDate='
-        self.wait = WebDriverWait(self.browser, 1.5)
 
-    def crawl(self, date=None, url=None):
-        if date:
-            try:
-                urls = self.get_targets(date)
-                print("start crawling", date)
-            except Exception as e:
-                with open("error.log", 'a') as err_file:
-                    log_text = str(date) + ", " + str(url) + ", " + str(e) + "\n"
-                    err_file.write(log_text)
-                self.browser.quit()
-                self.browser = webdriver.Firefox()
-                return
-        elif url:
-            urls = [url]
-        else:
-            urls = []
 
-        
-        for url in urls:
+    def __del__(self):
+        self.browser.quit()
+
+
+    def get_url_from_date(self, date):
+        try:
+            urls = self.get_targets(date)
+            return [(str(date), url) for url in urls]
+        except Exception as e:
+            with open("error.log", 'a') as err_file:
+                log_text = str(date) + ", " + str(url) + ", " + str(e) + "\n"
+                err_file.write(log_text)
             self.browser.quit()
             self.browser = webdriver.Firefox()
-            try:
-                self.browser.get(url)
-
-                # remove vod bar
-                self.browser.execute_script("""
-                var e = document.querySelector(".vod_open");
-                if (e)
-                    e.parentNode.removeChild(e);
-                """)
-
-                print('crawling', url)
-
-                news = self.parse_news(url)
-                print('news article parsed')
-                news['comment'] = {}
-
-                print('start scroll to end of page')
-                self.scroll_to_end()
-                print('\nscrolled to end of page')
-                print('start crawling news comment')
-                cmt_list = self.browser.find_elements_by_xpath("//ul[contains(@class, 'list_comment')]//li")
-                cmt_num = len(cmt_list)
-                for i, cmt in enumerate(cmt_list):
-                    data = self.parse_comment(cmt)
-                    if data:
-                        news['comment'][data['id']] = data
-                    print('\rcomment %d/%d is done' % (i+1, cmt_num), end='')
-                print('')
-
-                #write
-                json_data = json.dumps(news, ensure_ascii=False)
-                save_path = script_path + '/crawled_data/daum_news/' + str(date)
-                os.makedirs(save_path, exist_ok=True)
-                f = open(save_path + '/' + news['id'], 'w', encoding='utf-8')
-                f.write(json_data)
-                print(url, 'is crawled')
-            except Exception as e:
-                with open("error.log", 'a') as err_file:
-                    log_text = str(date) + ", " + str(url) + ", " + str(e) + "\n"
-                    err_file.write(log_text)
+            return []
 
 
-    def parse_news(self, url):
+    def crawl_url_and_save(browser, date, url):
+        try:
+            browser.get(url)
+
+            # remove vod bar
+            browser.execute_script("""
+            var e = document.querySelector(".vod_open");
+            if (e)
+                e.parentNode.removeChild(e);
+            """)
+
+            news = DaumCrawler._parse_news(browser, url)
+            news['comment'] = {}
+
+            DaumCrawler._scroll_to_end(browser)
+            cmt_list = browser.find_elements_by_xpath("//ul[contains(@class, 'list_comment')]//li")
+            cmt_num = len(cmt_list)
+            for i, cmt in enumerate(cmt_list):
+                data = DaumCrawler._parse_comment(cmt)
+                if data:
+                    news['comment'][data['id']] = data
+
+            #write
+            json_data = json.dumps(news, ensure_ascii=False)
+            save_path = SCRIPT_PATH + '/crawled_data/daum_news/' + str(date)
+            os.makedirs(save_path, exist_ok=True)
+            f = open(save_path + '/' + news['id'], 'w', encoding='utf-8')
+            f.write(json_data)
+        except Exception as e:
+            with open("error.log", 'a') as err_file:
+                log_text = str(date) + ", " + str(url) + ", " + str(e) + "\n"
+                err_file.write(log_text)
+        finally:
+            browser.quit()
+
+
+    def _parse_news(browser, url):
         news_id = url.split('/')[-1]
-        news_title = self.browser.find_element_by_xpath("//h3[contains(@class, 'tit_view')]").text
+        news_title = browser.find_element_by_xpath("//h3[contains(@class, 'tit_view')]").text
         news_open_time = None
         news_modi_time = None
         news_reporter = None
-        for txt_info in self.browser.find_elements_by_xpath("//span[contains(@class, 'info_view')]//span[contains(@class, 'txt_info')]"):
+        for txt_info in browser.find_elements_by_xpath("//span[contains(@class, 'info_view')]//span[contains(@class, 'txt_info')]"):
             info = txt_info.text
             if info[0] == '입' and info[1] == '력': news_open_time = info[2:]
             elif info[0] == '수' and info[1] == '정': news_modi_time = info[2:]
             else : news_reporter = info
-        news_press = self.browser.find_element_by_xpath("//div[contains(@class, 'head_view')]//img").get_attribute('alt')
-        news_body = self.browser.find_element_by_xpath("//div[contains(@class, 'article_view')]").text
+        news_press = browser.find_element_by_xpath("//div[contains(@class, 'head_view')]//img").get_attribute('alt')
+        news_body = browser.find_element_by_xpath("//div[contains(@class, 'article_view')]").text
         news = {'type' : 'news', 'id' : news_id, 'title' : news_title, 'time' : news_open_time, 'modi_time' : news_modi_time, 'press' : news_press, 'reporter' : news_reporter, 'text' : news_body }
         return news
 
-    def scroll_to_end(self):
+    def _scroll_to_end(browser):
         try:
-            i = 0
-            more_box = self.browser.find_element_by_css_selector("div.cmt_box>div.alex_more a")
+            more_box = browser.find_element_by_css_selector("div.cmt_box>div.alex_more a")
             box_loc = more_box.location
             while True:
-                i += 1
                 more_box.click()
-                print('\r' + str(i) + ' click()', end='')
                 start_time = time.time()
                 while len(more_box.find_elements_by_tag_name('span')) < 2:
                     if time.time()-start_time >= 10:
                         return
                     time.sleep(0.2)
-                    more_box = self.browser.find_element_by_css_selector("div.cmt_box>div.alex_more a")
+                    more_box = browser.find_element_by_css_selector("div.cmt_box>div.alex_more a")
                 new_loc = more_box.location
                 if box_loc == new_loc:
                     return
@@ -122,7 +109,7 @@ class DaumCrawler:
         except (NoSuchElementException, StaleElementReferenceException):
             return
 
-    def open_reply(self, comment):
+    def _open_reply(comment):
         try:
             reply_btn = comment.find_element_by_css_selector("div.box_reply button.reply_count span.num_txt")
             reply_btn.click()
@@ -150,7 +137,6 @@ class DaumCrawler:
     def get_targets(self, date):
         query = str(date)
         url = self.base_url + query
-#url = "http://media.daum.net/ranking/kkomkkom/"
         self.browser.get(url)
 
         li_list = self.browser.find_elements_by_xpath("//ul[contains(@class, 'list_news2')]//li")
@@ -161,7 +147,7 @@ class DaumCrawler:
             urls.append(tag_a.get_attribute("href"))
         return urls
 
-    def parse_comment(self, comment, is_reply=False):
+    def _parse_comment(comment, is_reply=False):
         data = {}
 
         try:
@@ -198,47 +184,49 @@ class DaumCrawler:
             data['like'] = int(comment.find_element_by_css_selector('button.btn_recomm span.num_txt').text)
             data['dislike'] = int(comment.find_element_by_css_selector('button.btn_oppose span.num_txt').text)
 
-            if self.open_reply(comment):
+            if DaumCrawler._open_reply(comment):
                 data['reply'] = {}
                 reply_list = comment.find_elements_by_css_selector('ul.list_reply li')
                 for reply in reply_list:
-                    r_data = self.parse_comment(reply, is_reply=True)
+                    r_data = DaumCrawler._parse_comment(reply, is_reply=True)
                     if r_data:
                         data['reply'][r_data['id']] = r_data
 
         return data
 
-def get_date_to_crawl():
+def get_urls_to_crawl(crawler):
     parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--date', nargs='+', help='date to crawl. the format is YYYYMMDD. ex)20180211')
-    group.add_argument('-u', '--url', help='make \'date\' parameter to get a url')
-    group.add_argument('--duration', nargs=2, help='crawling news between two dates')
+    parser.add_argument('--date', nargs='+', help='date to crawl. the format is YYYYMMDD. ex)20180211')
+    parser.add_argument('-u', '--url', help='make \'date\' parameter to get a url')
+    parser.add_argument('--duration', nargs=2, help='crawling news between two dates')
     args = parser.parse_args()
+
+    urls = []
     if args.url:
-        return ('url', args.url)
-    elif args.duration:
+        urls.append(('',args.url))
+
+    dates = []
+    if args.duration:
         d_first = datetime.datetime.strptime(args.duration[0], "%Y%m%d").date()
         d_last = datetime.datetime.strptime(args.duration[1], "%Y%m%d").date()
-        dates = []
         while d_first <= d_last:
             dates.append(d_first.strftime("%Y%m%d"))
             d_first += datetime.timedelta(days=1)
-        return ('date', dates)
-    else:
-        return ('date', [d for d in args.date])
+    if args.date:
+        dates += args.date
+
+    print('%s will be crawled' % ','.join(dates))
+    for date in dates:
+        urls += crawler.get_url_from_date(date)
+
+    return urls
 
 if __name__ == '__main__':
-    dt = get_date_to_crawl()
+    os.environ['MOZ_HEADLESS'] = '1'
     dc = DaumCrawler()
-    if dt[0] == 'url':
-        dc.crawl(url=dt[1])
-    elif dt[0] == 'date':
-        print("articles on following dates is going to be crawled: ")
-        for d in dt[1]:
-            print(d, end=', ')
-        print("")
-        for d in dt[1]:
-            dc.crawl(date=d)
+    urls = get_urls_to_crawl(dc)
     
-    dc.browser.quit()
+    print('0/%d has crawled' % len(urls))
+    for i, url in enumerate(urls):
+        DaumCrawler.crawl_url_and_save(webdriver.Firefox(), url[0], url[1])
+        print('%d/%d has crawled' % (i, len(urls)))
