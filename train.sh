53bdd5e1beb510e4ef69aece3908f18a0fcd1e87
Jae-Yong Choi <jayong93@gmail.com>
2018-07-20 13:29:31 +0900

    char-rnn 학습과 nmt 학습을 직렬화

--- train.sh
+++ train.sh
@@ -7,6 +7,7 @@ FILTER_DIR=${SCRIPT_DIR}/DaumFilter
 CHAR_RNN_DIR=${SCRIPT_DIR}/kor-char-rnn-tensorflow
 NMT_DIR=${SCRIPT_DIR}/nmt
 CRAWL_DATE=$(date '+%Y%m%d' -d "yesterday")
+TODAY=$(date '+%Y-%m-%d')
 
 # CharRNN 학습 데이터 준비
 echo "=== Start Crawling ${CRAWL_DATE} ==="
@@ -17,22 +18,15 @@ echo -e "${CRAWLED_DATA_DIR}/${CRAWL_DATE}" | ${FILTER_DIR}/make_data.sh
 
 echo "=== Copying Filtered Data ==="
 mkdir -p ${CHAR_RNN_DIR}/data/news
-rm ${CHAR_RNN_DIR}/data/news/*
 cp ${FILTER_DIR}/output/char_rnn_data.txt ${CHAR_RNN_DIR}/data/news/input.txt
 
 # CharRNN 기존 모델 백업
-tar -czf ${CHAR_RNN_DIR}/save/news.bak.tgz ${CHAR_RNN_DIR}/save/news/
+tar -czf "${CHAR_RNN_DIR}/save/news.bak-${TODAY}.tgz" -C ${CHAR_RNN_DIR}/save news/
 
 # NMT 학습 데이터 준비
 NMT_ADDTIONAL_DATE=$(date '+%Y%m%d' -d "2 day ago")
 echo -e "${CRAWLED_DATA_DIR}/${CRAWL_DATE}\n${CRAWLED_DATA_DIR}/${NMT_ADDTIONAL_DATE}" | ${FILTER_DIR}/make_data.sh
-cp ${FILTER_DIR}/output/*.comment ${NMT_DIR}/train/
-cp ${FILTER_DIR}/output/*.title ${NMT_DIR}/train/
 
 echo "=== Start Learning ==="
-${CHAR_RNN_DIR}/train.sh --init_from ${CHAR_RNN_DIR}/save/news &
-PID1=$!
-${NMT_DIR}/train.sh &
-PID2=$!
-
-wait ${PID1} ${PID2}
+cd ${CHAR_RNN_DIR} && ${CHAR_RNN_DIR}/train.sh --init_from ${CHAR_RNN_DIR}/save/news
+${NMT_DIR}/train.sh
