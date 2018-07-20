6dfb24385e8f43d69decca0204d346f865ac5b74
Jae-Yong Choi <jayong93@gmail.com>
2018-07-20 20:32:11 +0900

    tensorflow restore의 구현 변경에 맞춰 수정

--- nmt/infer.sh
+++ nmt/infer.sh
@@ -9,6 +9,7 @@ INPUT_FILE=$(readlink -f "${INPUT_FILE}")
 OUTPUT_FILE=$(readlink -f "${OUTPUT_FILE}")
 MODEL_DIR=$(readlink -f ${MODEL_DIR})
 CKPT_FILE=$(ls ${MODEL_DIR} | grep "ckpt.\+\.index" | sort -V -r | head -1)
+CKPT_FILE=${CKPT_FILE%'.index'}
 
 SOURCE_DIR=$(cd $(dirname "$0") && pwd)
 VOLUME_DIR=/nmt
@@ -18,7 +19,7 @@ OUTPUT_FILE=${OUTPUT_FILE//${SOURCE_DIR}/${VOLUME_DIR}}
 MODEL_DIR=${MODEL_DIR//${SOURCE_DIR}/${VOLUME_DIR}}
 
 
-docker run --rm -v ${SOURCE_DIR}:${VOLUME_DIR} tensorflow/tensorflow:nightly-devel-py3 bash -c "cd /nmt && python3 -m nmt.nmt \
+nvidia-docker run --rm -v ${SOURCE_DIR}:${VOLUME_DIR} tensorflow/tensorflow:nightly-devel-gpu-py3 bash -c "export PYTHONIOENCODING=UTF-8 && cd /nmt && python3 -m nmt.nmt \
     --src=title --tgt=comment \
     --ckpt=${MODEL_DIR}/${CKPT_FILE} \
     --out_dir=${MODEL_DIR} \
