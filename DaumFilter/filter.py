2e0c8eec08dbd27f5fcfa2a5aa70e6306d332505
ssy <thdbsdox12@gmail.com>
2018-07-23 16:15:58 +0900

    DaumFilter로 학습 데이터 만들 때 댓글 갯수 제한 가능

--- DaumFilter/filter.py
+++ DaumFilter/filter.py
@@ -8,10 +8,11 @@ def add_arguments(arg_parser):
     arg_parser.add_argument("archive", nargs="*", help="archives to be filtered")
     arg_parser.add_argument("--dislike_multiplier", type=float, default=2, help="a number to be multiply to dislike count")
     arg_parser.add_argument("-o", "--out_to", type=argparse.FileType('w', encoding='utf-8'), help="a file to include this program's output") 
+    arg_parser.add_argument("-m", "--max_cmt", type=int, default=0, help="maximum number of comment to be filtered")
     return arg_parser
 
 # 걸러진 댓글들을 반환하는 함수
-def get_filtered_list(archive_file, dislike_multiplier):
+def filter_comment(archive_file, dislike_multiplier):
     archive_dict = json.load(archive_file)
     title = archive_dict["title"]
     comment_list = get_comment_list(archive_dict)
@@ -19,6 +20,20 @@ def get_filtered_list(archive_file, dislike_multiplier):
     result = sorted(result, key=lambda o: o["like"], reverse=True)
     result_len = len(result)*0.1
     return {"title":title, "comments":[cmt["text"] for cmt in result[:int(result_len)]]}
+
+
+def limit_comments_num(comments, max_num):
+    result = []
+    for cmt_dict in comments:
+        if len(result)+len(cmt_dict["comments"]) < max_num:
+            result.append(cmt_dict)
+        else:
+            cmt_dict["comments"] = cmt_dict["comments"][:max_num-len(result)]
+            result.append(cmt_dict)
+            break
+
+    return result
+    
 
 # 전체 기록에서 댓글만 반환하는 함수
 def get_comment_list(archive_dict):
@@ -51,7 +66,9 @@ if __name__ == "__main__":
     else:
         parser.error("The input archives are not directories or files")
 
-    result = [get_filtered_list(open(archive), args.dislike_multiplier) for archive in archives]
+    result = [filter_comment(open(archive), args.dislike_multiplier) for archive in archives]
+    if args.max_cmt > 0:
+        result = limit_comments_num(result, args.max_cmt)
     result_json = json.dumps(result, ensure_ascii=False)
     if args.out_to == None:
         print(result_json)
