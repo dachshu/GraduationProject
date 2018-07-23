a31b0120b031a0f1548834c3b4888f3e37d3d28f
Jae-Yong Choi <jayong93@gmail.com>
2018-07-23 16:15:58 +0900

    DaumFilter로 학습 데이터 만들 때 댓글 갯수 제한 가능

--- DaumFilter/make_data.sh
+++ DaumFilter/make_data.sh
@@ -4,8 +4,12 @@ SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 OUT_DIR=${SCRIPT_DIR}/output
 OUT_JSON_NAME=${OUT_DIR}/output.json
 
+if [ ! -z $1 ]; then
+    ADDITIONAL_CMD="-m $1"
+fi
+
 mkdir -p ${OUT_DIR}
-${SCRIPT_DIR}/filter.py ${ARCHIVES_DIR} -o ${OUT_JSON_NAME} 0<&0
+${SCRIPT_DIR}/filter.py ${ARCHIVES_DIR} -o ${OUT_JSON_NAME} ${ADDITIONAL_CMD} 0<&0
 $(cd ${SCRIPT_DIR} && python3 process_data.py && mv *.txt -t ${OUT_DIR})
 sed -i "/^$/d" ${OUT_DIR}/*.txt
 ${SCRIPT_DIR}/separate_dataset.py ${OUT_DIR}/title.txt ${OUT_DIR}/comment.txt ${OUT_DIR}/vocab.txt --out_dir ${OUT_DIR}
