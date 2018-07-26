4279a9233795530db461ed2a7959e9c3bea90f51
Jae-Yong Choi <jayong93@gmail.com>
2018-07-26 11:23:26 +0900

    auto 스크립트에서 generation에 대한 로그를 저장

--- auto.sh
+++ auto.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+TODAY=$(date '+%Y-%m-%d')
 
 ${SCRIPT_DIR}/train.sh
-${SCRIPT_DIR}/generate.sh
+mkdir -p ${SCRIPT_DIR}/log && ${SCRIPT_DIR}/generate.sh | tee ${SCRIPT_DIR}/log/generation-log-${TODAY}
