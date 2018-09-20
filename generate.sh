6b106056cdca6767803f88340ab3df03c9e3f065
Jae-Yong Choi <jayong93@gmail.com>
2018-09-20 21:58:26 +0900

    댓글 생성에 at 사용하도록 변경

--- generate.sh
+++ generate.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
-CHAR_RNN_DIR=${SCRIPT_DIR}/kor-char-rnn-tensorflow
-NMT_DIR=${SCRIPT_DIR}/nmt
-TWEET_UPLOADER_DIR=${SCRIPT_DIR}/TweetUploader
-PARTIAL_PRINTER_DIR=${SCRIPT_DIR}/partial_print
 
 echo "=== Generating Times ==="
 TIME_GENERATOR_DIR=${SCRIPT_DIR}/CommentTimeGenerator
@@ -26,29 +22,8 @@ for t in ${GENERATED_TIMES}; do
         continue
     fi
 
-    PERIOD_TO_SLEEP=$((TARGET_TIME-CURRENT_TIME))
-    echo "Sleep for ${PERIOD_TO_SLEEP}s"
-    sleep ${PERIOD_TO_SLEEP}
-
-    # get a news article
-    ARTICLE_TITLES_LINKS=$(${SCRIPT_DIR}/GetDaumMainNews.py)
-    INDEX=$(shuf -i 1-5 -n 1)
-    SELECTED_TITLE=$(echo "${ARTICLE_TITLES_LINKS}" | awk "NR == (${INDEX}*2-1)")
-    SELECTED_LINK=$(echo "${ARTICLE_TITLES_LINKS}" | awk "NR == ${INDEX}*2")
-    echo -e "${SELECTED_TITLE}\n${SELECTED_LINK}\n" > /tmp/generated_comments.txt
-
-    # generate comments of the article via char-rnn and seq2seq model
-    echo "Generate a comment from the title: ${SELECTED_TITLE}"
-    echo "${SELECTED_TITLE}" > ${CHAR_RNN_DIR}/input_for_generation.txt
-    python3 ${CHAR_RNN_DIR}/sample.py --save_dir ${CHAR_RNN_DIR}/save/news --prime "$(head -1 ${CHAR_RNN_DIR}/input_for_generation.txt)" \
-        --output_file /tmp/char_rnn_infer_output.txt && \
-    cat /tmp/char_rnn_infer_output.txt >> /tmp/generated_comments.txt
-
-    echo "${SELECTED_TITLE}" > ${NMT_DIR}/input_for_generation.txt
-    ${NMT_DIR}/infer.sh ${NMT_DIR}/save/model ${NMT_DIR}/input_for_generation.txt ${NMT_DIR}/infer_output.txt && \
-    cat ${NMT_DIR}/infer_output.txt >> /tmp/generated_comments.txt
-
-    cat /tmp/generated_comments.txt | ${PARTIAL_PRINTER_DIR}/partial_print.py -140 | ${TWEET_UPLOADER_DIR}/TweetUploader.py -k ${TWEET_UPLOADER_DIR}/twitter_key
-    ( cd ${TIME_GENERATOR_DIR} && echo "${GENERATED_TIMES}" | tail -1 > latest_generated_time )
+    $(at ${HOUR}:${MINITE} -f ${SCRIPT_DIR}/generate_comment.sh)
 done
+
+( cd ${TIME_GENERATOR_DIR} && echo "${GENERATED_TIMES}" | tail -1 > latest_generated_time )
 
