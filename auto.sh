927f901eaaeccd83e1a7a5118589eb2507af1dc1
Jae-Yong Choi <jayong93@gmail.com>
2018-10-11 19:09:49 +0900

    학습 로그 저장 방식 변경

--- auto.sh
+++ auto.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 TODAY=$(date '+%Y-%m-%d')
 
 mkdir -p ${SCRIPT_DIR}/log/${TODAY}
-${SCRIPT_DIR}/train.sh 2>&1 | tee ${SCRIPT_DIR}/log/${TODAY}/train-log
-${SCRIPT_DIR}/generate.sh 2>&1 | tee ${SCRIPT_DIR}/log/${TODAY}/generation-log
+${SCRIPT_DIR}/train.sh
+${SCRIPT_DIR}/generate.sh
