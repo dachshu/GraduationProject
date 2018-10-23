f3600be10d653db31b03b721df2f31c8736d1174
Jae-Yong Choi <jayong93@gmail.com>
2018-10-23 14:46:55 +0900

    다음 뉴스 크롤링 변경사항 적용

--- GetDaumMainNews.py
+++ GetDaumMainNews.py
@@ -9,7 +9,7 @@ def get_article_title(url):
     rq = requests.get(url)
     soup = BeautifulSoup(rq.text, 'html.parser')
     if 'm.media' in url:
-        article = soup.select('ul.list_mainnews li.selected a')
+        article = soup.select('div.item_mainnews a.link_cont')
         ret_url = article[0]['href']
         rq = requests.get(ret_url)
         soup = BeautifulSoup(rq.text, 'html.parser')
