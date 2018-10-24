1c1ea74049d475433193239d9163000793a1b1e1
Choi Jae Yong <jayong93@gmail.com>
2018-10-24 22:47:57 +0900

    fixed training script to access correct path of python script file

--- bin/train_char_rnn.sh
+++ bin/train_char_rnn.sh
@@ -13,4 +13,6 @@ if [ -d "${OLD_MODEL_DIR}" ]; then
     OPTION="--init_from ${OLD_MODEL_DIR}"
 fi
 
-python3 train.py --data_dir "${INPUT_DIR}" --save_dir "${OUTPUT_DIR}" --rnn_size 1024 --num_layers 3 --num_epochs 100 --batch_size 32 "${OPTION}"
+PROJECT_DIR=$(cd $(dirname "$0")/.. && pwd)
+CHAR_RNN_DIR="${PROJECT_DIR}/kor-char-rnn-tensorflow"
+python3 "${CHAR_RNN_DIR}/train.py" --data_dir "${INPUT_DIR}" --save_dir "${OUTPUT_DIR}" --rnn_size 1024 --num_layers 3 --num_epochs 100 --batch_size 32 "${OPTION}"
