11b6ef3c58a2572c1fdac459c053845a22d15588
ssy <thdbsdox12@gmail.com>
2018-11-06 22:43:38 +0900

    implemented showstat script

--- bin/showstat.py
+++ bin/showstat.py
@@ -1,14 +1,45 @@
 import argparse
+import os
+from os import path
 
 def parse_arguments(parser):
     parser.add_argument("log_dir", help="Specifies a directory where log files are.")
     parser.add_argument("-a", "--all", type=bool, help="Shows entire status, not current status.")
-    return parser.parse_args()
+    args = parser.parse_args()
+    if not path.is_dir(args.log_dir):
+        parser.error("LOG_DIR is not a directory")
+    return args
+
+
+def print_at_job_log(time, at_log, show_all):
+    if show_all:
+        print("Time: " + time)
+        print("Title: " + at_log[0].strip())
+        print("URL: " + at_log[1].strip())
+        print("Comment1: " + at_log[2].strip())
+        print("Comment2: " + at_log[3].strip())
+    else:
+        print("Time: " + time)
+        print("Title: " + at_log[0].strip())
+        print("Comment1: " + at_log[2].strip())
+        print("Comment2: " + at_log[3].strip())
+
 
 
 def show_status(log_dir, show_all):
-    if show_all:
-        pass
+    with open(path.join(log_dir, "general.log"), 'r') as log:
+        if show_all:
+            print(log.read())
+        else:
+            print(log.readlines()[-1])
+
+        for root, _, files in os.walk(path.join(log_dir, "detail", "upload_comment_tweet")):
+            for f_name in files:
+                with open(path.join(root,f_name), 'r') as at_log:
+                    # title, url, comment1, comment2
+                    info = at_log.readlines()[-4:]
+                    t, _ = path.splitext(f_name)
+                    print_at_job_log(t, info, show_all)
 
 
 if __name__ == "__main__":
