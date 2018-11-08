316c2371cf3e04aa6f69e3b5142641e19ace7e4e
ssy <thdbsdox12@gmail.com>
2018-11-08 15:37:46 +0900

    change current directory to \'bin\' before run generation scripts

--- bin/generate_comment_tweet_with_log.sh
+++ bin/generate_comment_tweet_with_log.sh
@@ -10,5 +10,6 @@ DETAIL_LOG_PATH="${DETAIL_LOG_DIR}/${TIME}.log"
 mkdir -p ${DETAIL_LOG_DIR}
 touch "${DETAIL_LOG_PATH}"
 
+cd "${PROJECT_DIR}/bin"
 ./generate_comment_tweet.sh > ${DETAIL_LOG_PATH} 2>&1
 
