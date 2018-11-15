3737781171266f949b40c421575798cbaf19901c
Jae-Yong Choi <jayong93@gmail.com>
2018-11-15 14:33:52 +0900

    commited by soyun

--- bin/generate_charRNN_comment.sh
+++ bin/generate_charRNN_comment.sh
@@ -4,10 +4,5 @@ NEWS_TITLE=$1
 DIR_NAME="$(echo "${NEWS_TITLE}" | awk '{printf "%s", $1}')_$(date +%T)_charRNN"
 #DIR_NAME=$(echo "${NEWS_TITLE}" | md5sum | awk '{printf "%s_charRNN", $1}')
 TODAY=$(date '+%Y-%m-%d')
-IN_OUT_DIR=$(echo "../results/${TODAY}/${DIR_NAME}")
 MODEL_DIR=$(echo "../kor-char-rnn-tensorflow")
-mkdir -p ${IN_OUT_DIR}
-echo "${NEWS_TITLE}" > ${IN_OUT_DIR}/input.txt
-python3 ${MODEL_DIR}/sample.py --save_dir ${MODEL_DIR}/save/news --prime "$(head -1 ${IN_OUT_DIR}/input.txt)" \
-    --output_file "${IN_OUT_DIR}/output.txt" 
-echo "[output dir name] ${IN_OUT_DIR}"
+python3 ${MODEL_DIR}/sample.py --save_dir ${MODEL_DIR}/save/news --prime "$NEWS_TITLE"
--- bin/generate_comment_tweet.sh
+++ bin/generate_comment_tweet.sh
@@ -4,38 +4,29 @@ TODAY="$(date '+%Y-%m-%d')"
 
 # get a daum main news's title and url
 echo "[GET A DAUM MAIN NEWS'S TITLE AND URL]"
-NEWS_TITLE_N_URL=$(./get_daum_main_news.sh)
-NEWS_TITLE=$(echo "${NEWS_TITLE_N_URL}" | head -1)
-NEWS_URL=$(echo "${NEWS_TITLE_N_URL}" | tail -1)
+ARTICLE_URLS=$(./GetDaumMainNews.py)
+NUM_URLS=$(echo "$ARTICLE_URLS" | wc -l)
+INDEX=$(shuf -i 1-$NUM_URLS -n 1)
+NEWS_URL=$(echo "$ARTICLE_URLS" | awk "NR == $INDEX")
+NEWS_TITLE=$(./get_daum_news_title.py "$NEWS_URL")
 echo "selected news : ${NEWS_TITLE}"
-
-# tweet text file
-echo ""
-echo "[OUTPUT TWEET TEXT FILE]"
-OUTPUT_FILE_NAME="$(echo "${NEWS_TITLE}" | awk '{printf "%s", $1}')_$(date +%T)_comment_tweet.txt"
-#OUTPUT_FILE_NAME=$(echo "${NEWS_TITLE}" | md5sum | awk '{printf "%s_comment_tweet.txt", $1}')
-OUTPUT_FILE_PATH=$(echo "../results/${TODAY}/${OUTPUT_FILE_NAME}")
-echo ${OUTPUT_FILE_PATH}
-echo -e "${NEWS_TITLE_N_URL}\n" > "${OUTPUT_FILE_PATH}"
 
 # generate comment of charRnn model
 echo ""
 echo "[GENERATE COMMENT OF CHAR RNN MODEL]"
-CHAR_RNN_OUTPUT_DIR=$(./generate_charRNN_comment.sh "${NEWS_TITLE}"| tail -1 | awk '{printf $4}')
-echo ${CHAR_RNN_OUTPUT_DIR}
-cat "${CHAR_RNN_OUTPUT_DIR}/output.txt" >> ${OUTPUT_FILE_PATH}
+CHAR_RNN_OUTPUT="$(echo "$(./generate_charRNN_comment.sh "${NEWS_TITLE}")")"
+echo "${CHAR_RNN_OUTPUT}"
 
 # generate commnet of nmt model
 echo ""
 echo "[GENERATE COMMENT OF NMT MODEL]"
-NMT_OUTPUT_DIR=$(./generate_nmt_comment.sh "${NEWS_TITLE}" | tail -1 | awk '{printf $4}')
-echo ${NMT_OUTPUT_DIR}
-cat "${NMT_OUTPUT_DIR}/output.txt" >> "${OUTPUT_FILE_PATH}"
+NMT_OUTPUT="$(echo "$(./generate_nmt_comment.sh "${NEWS_TITLE}" | tail -1)")"
+echo "${NMT_OUTPUT}"
 
 # tweet generated comments
 echo ""
 echo "[UPLOAD TWEET]"
-TWEET_TEXT=$(cat ${OUTPUT_FILE_PATH})
+TWEET_TEXT="$(echo -e "${NEWS_TITLE}\n${NEWS_URL}\n${CHAR_RNN_OUTPUT}\n${NMT_OUTPUT}")"
 ./upload_tweet.sh "${TWEET_TEXT}"
 
 echo ""
