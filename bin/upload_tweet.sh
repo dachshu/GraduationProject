f0747f10560000597ae778e1a030a790ac74728f
ssy <thdbsdox12@gmail.com>
2018-12-14 16:32:07 +0900

    add help message for some scripts

--- bin/upload_tweet.sh
+++ bin/upload_tweet.sh
@@ -1,9 +1,40 @@
 #!/bin/bash
 
-TWEET_TEXT=$1
-if [ -z "${TWEET_TEXT}" ]; then
+echoerr() {
+    echo "$@" 1>&2
+}
+
+function print_help() {
+    echoerr "usage: upload_tweet.sh [TEXT..]"
+    echoerr "   TEXT : some text which will be posted on twitter."
+    echoerr "       if no TEXT is provided, the script would read text from STDIN."
+    exit 1
+}
+
+POSITIONAL=()
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -h|--help)
+            print_help
+            shift
+            ;;
+        *)
+            POSITIONAL+=("$1")
+            shift
+            ;;
+    esac
+done
+
+if [ ${#POSITIONAL[@]} -lt 1 ]; then
     TWEET_TEXT=$(cat)
+else
+    TWEET_TEXT=${POSITIONAL[0]}
+    for t in ${POSITIONAL[@]:1}; do
+        TWEET_TEXT="${TWEET_TEXT} $t"
+    done
 fi
+
 TWEET_UPLOADER_DIR="../TweetUploader"
 PARTIAL_PRINTER_DIR="../partial_print"
 
