6324f67b54c2c5268efa269d2b64950c9f572a8f
Choi Jae Yong <jayong93@gmail.com>
2019-01-03 23:01:55 +0900

    fix a bug that the enqueue script doesn't register jobs

--- bin/enq_generation_at_job.sh
+++ bin/enq_generation_at_job.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 GENERATION_TIME=$1
-at -q d ${GENERATION_TIME} -f "${SCRIPT_DIR}/generate_comment_tweet_with_log.sh"
+echo "bash ${SCRIPT_DIR}/generate_comment_tweet_with_log.sh" | at -q d ${GENERATION_TIME}
 
