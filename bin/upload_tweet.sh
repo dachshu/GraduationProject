ee88fd05ca27b1b5b92be506c4e1ff1e75369598
Jae Yong Choi <jayong93@gmail.com>
2019-01-07 15:12:50 +0900

    upload generated comment without truncating

--- bin/upload_tweet.sh
+++ bin/upload_tweet.sh
@@ -36,6 +36,5 @@ else
 fi
 
 TWEET_UPLOADER_DIR="../TweetUploader"
-PARTIAL_PRINTER_DIR="../partial_print"
 
-echo -e "${TWEET_TEXT}" | ${PARTIAL_PRINTER_DIR}/partial_print.py -140 | ${TWEET_UPLOADER_DIR}/TweetUploader.py -k ${TWEET_UPLOADER_DIR}/twitter_key
+echo -e "${TWEET_TEXT}" | ${TWEET_UPLOADER_DIR}/TweetUploader.py -k ${TWEET_UPLOADER_DIR}/twitter_key
