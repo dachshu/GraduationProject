1c18f0f1efe825f91dfb3b9700c08c423ecebc70
Jae Yong Choi <jayong93@gmail.com>
2019-02-01 15:01:32 +0900

    nmt 추론 스크립트의 입력, 출력 디렉토리를 개별적으로 설정할 수 있도록 변경

--- nmt/infer.sh
+++ nmt/infer.sh
@@ -13,20 +13,21 @@ CKPT_FILE=${CKPT_FILE%'.index'}
 
 SOURCE_DIR=$(cd $(dirname "$0") && pwd)
 VOLUME_DIR=/nmt
-IN_OUT_DIR=$(cd "../results" && pwd)
-INNER_IN_OUT_DIR=/results
+INPUT_DIR=$(cd $(dirname "${INPUT_FILE}") && pwd)
+INNER_INPUT_DIR=/input
+OUTPUT_DIR=$(cd $(dirname "${OUTPUT_FILE}") && pwd)
+INNER_OUTPUT_DIR=/output
+INNER_MODEL_DIR=/nmt_output # 모델의 hparam에 training 할때의 경로가 저장되므로 똑같이 맞춰줘야 함.
+INNER_VOCAB_DIR=/nmt_input
 
-INPUT_FILE=${INPUT_FILE//${IN_OUT_DIR}/${INNER_IN_OUT_DIR}}
-OUTPUT_FILE=${OUTPUT_FILE//${IN_OUT_DIR}/${INNER_IN_OUT_DIR}}
-MODEL_DIR=${MODEL_DIR//${SOURCE_DIR}/${VOLUME_DIR}}
+INPUT_FILE=${INPUT_FILE//${INPUT_DIR}/${INNER_INPUT_DIR}}
+OUTPUT_FILE=${OUTPUT_FILE//${OUTPUT_DIR}/${INNER_OUTPUT_DIR}}
 
-nvidia-docker run --rm -v ${SOURCE_DIR}:${VOLUME_DIR} -v "${IN_OUT_DIR}:${INNER_IN_OUT_DIR}" tensorflow/tensorflow:nightly-devel-gpu-py3 bash -c "export PYTHONIOENCODING=UTF-8 && cd /nmt && python3 -m nmt.nmt \
+nvidia-docker run --rm -v "${SOURCE_DIR}:${VOLUME_DIR}" -v "${INPUT_DIR}:${INNER_INPUT_DIR}" -v "${OUTPUT_DIR}:${INNER_OUTPUT_DIR}" -v "${MODEL_DIR}:${INNER_MODEL_DIR}" -v "${MODEL_DIR}:${INNER_VOCAB_DIR}" tensorflow/tensorflow:nightly-devel-gpu-py3 bash -c "export PYTHONIOENCODING=UTF-8 && cd ${VOLUME_DIR} && python3 -m nmt.nmt \
     --src=title --tgt=comment \
-    --ckpt=${MODEL_DIR}/${CKPT_FILE} \
-    --out_dir=${MODEL_DIR} \
-    --vocab_prefix=${VOLUME_DIR}/train \
-    --share_vocab \
-    --inference_input_file="${INPUT_FILE}" \
-    --inference_output_file="${OUTPUT_FILE}""
+    --ckpt=${INNER_MODEL_DIR}/${CKPT_FILE} \
+    --out_dir=${INNER_MODEL_DIR} \
+    --inference_input_file=${INPUT_FILE} \
+    --inference_output_file=${OUTPUT_FILE}"
 
 exit $?
