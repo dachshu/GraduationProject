6b76c4bcfb6aa8353579310ef0e1ff0b934b154d
Jae Yong Choi <jayong93@gmail.com>
2019-02-19 11:11:41 +0900

    added a part of transformer in the generation script

--- bin/generate_comment_tweet.sh
+++ bin/generate_comment_tweet.sh
@@ -10,6 +10,11 @@ function exit_if_err() {
 }
 
 TODAY="$(date '+%Y-%m-%d')"
+###################################################################
+TRANS_MODEL_DIR="../transformer_out"
+TRANS_VOCAB_FILE="../transformer_out/vocab"
+###################################################################
+
 
 # get a daum main news's title and url
 echo "[GET A DAUM MAIN NEWS'S TITLE AND URL $(date +"%T")]"
@@ -24,23 +29,33 @@ echo "selected news $(date +"%T") : ${NEWS_TITLE}"
 
 
 # generate comment of charRnn model
+char_rnn_output=""
 echo ""
-echo "[GENERATE COMMENT OF CHAR RNN MODEL $(date +"%T")]"
-CHAR_RNN_OUTPUT="$(echo "$(./generate_charRNN_comment.sh "${NEWS_TITLE}")")"
-exit_if_err "generate char rnn comment $(date +"%T")"
-echo "${CHAR_RNN_OUTPUT}"
+echo "[generate comment of char rnn model $(date +"%t")]"
+char_rnn_output="$(echo "$(./generate_charrnn_comment.sh "${news_title}")")"
+exit_if_err "generate char rnn comment $(date +"%t")"
+echo "${char_rnn_output}"
 
 # generate commnet of nmt model
+NMT_OUTPUT=""
 echo ""
 echo "[GENERATE COMMENT OF NMT MODEL $(date +"%T")]"
 NMT_OUTPUT="$(echo "$(./generate_nmt_comment.sh "${NEWS_TITLE}" | tail -1)")"
 exit_if_err "generate nmt comment $(date +"%T")"
 echo "${NMT_OUTPUT}"
 
+# generate comment of transformer model
+TRANSFORMER_OUTPUT=""
+echo ""
+echo "[GENERATE COMMENT OF TRANSFORMER MODEL $(date +"%T")]"
+TRANSFORMER_OUTPUT="$(echo "$(./generate_transformer_comment.sh ${TRANS_MODEL_DIR} ${TRANS_VOCAB_FILE} "${NEWS_TITLE}")")"
+exit_if_err "generate transformer comment $(date +"%T")"
+echo "${TRANSFORMER_OUTPUT}"
+
 # tweet generated comments
 echo ""
 echo "[UPLOAD TWEET $(date +"%T")]"
-TWEET_TEXT="$(echo -e "${NEWS_TITLE}\n${NEWS_URL}\n${CHAR_RNN_OUTPUT}\n\n${NMT_OUTPUT}")"
+TWEET_TEXT="$(echo -e "${NEWS_TITLE}\n${NEWS_URL}\n${CHAR_RNN_OUTPUT}\n${NMT_OUTPUT}\n${TRANSFORMER_OUTPUT}")"
 ./upload_tweet.sh "${TWEET_TEXT}"
 exit_if_err "upload tweet $(date +"%T")"
 
