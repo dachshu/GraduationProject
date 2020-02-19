5f7e092e6d4cf45b147bcbdae60cf00b450eb822
ssy <thdbsdox12@gmail.com>
2020-02-20 00:56:37 +0900

    add an option to print time of comments written

--- bin/naver_news_filter.py
+++ bin/naver_news_filter.py
@@ -8,10 +8,11 @@ def add_arguments(arg_parser):
     arg_parser.add_argument("archive", nargs="*", help="archives to be filtered")
     arg_parser.add_argument("-c", "--max-comment-num", type=int, default=100, help="a number that commments will be filtered")
     arg_parser.add_argument("--out-plain-text", action="store_true", help="print comments as plain text, not as json structure")
+    arg_parser.add_argument("--with-time", action="store_true", help="print also times comments have been written")
     return arg_parser
 
 # 걸러진 댓글들을 반환하는 함수
-def filter_comment(archive_file, max_count):
+def filter_comment(archive_file, max_count, with_time):
     archive_dict = json.load(archive_file)
     title = archive_dict["title"].replace("\t", " ")
     comment_list = get_comment_list(archive_dict)
@@ -20,7 +21,11 @@ def filter_comment(archive_file, max_count):
     result_len = len(result)
     result_len = min(result_len, max_count)
 
-    result_dict = {"title":title, "comments":[cmt["text"].replace("\t", " ").replace("\n", " ") for cmt in result[:int(result_len)]]}
+    result_dict = {"title":title, "comments":[], "time": []}
+    for cmt in result[:int(result_len)]:
+        result_dict["comments"].append(cmt["text"].replace("\t", " ").replace("\n", " "))
+        if with_time:
+            result_dict["time"].append(int(float(cmt["time"])))
     if len(result_dict["comments"]) == 0:
         print("WARNING: a news item(\'%s\', in \'%s\') has no comments" % (title, archive_file.name), file=sys.stderr)
     return result_dict
@@ -34,7 +39,7 @@ def get_comment_list(archive_dict):
 def filter_stupid_news(news_file_path):
     archive = json.load(open(news_file_path))
     man_proportion = archive.get("man_proportion")
-    if man_proportion is None or int(man_proportion[:-1]) < 70:
+    if man_proportion is None or int(man_proportion[:-1]) < 60:
         return False
     
     age50_proportion = archive.get("age50_proportion")
@@ -68,10 +73,13 @@ if __name__ == "__main__":
     else:
         parser.error("The input archives are neither directories nor files")
 
-    result = [filter_comment(open(archive), args.max_comment_num) for archive in archives if filter_stupid_news(archive)]
+    result = [filter_comment(open(archive), args.max_comment_num, args.with_time) for archive in archives if filter_stupid_news(archive)]
     if args.out_plain_text:
         for r in result:
-            comments = [comment.replace("\n", "") for comment in r["comments"]]
+            if args.with_time:
+                comments = [str(time) + " " + comment.replace("\n", " ") for (comment, time) in zip(r["comments"], r["time"])]
+            else:
+                comments = [comment.replace("\n", " ") for comment in r["comments"]]
             print("\n".join(comments))
     else:
         print(json.dumps(result, ensure_ascii=False))
